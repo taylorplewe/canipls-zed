0d8706185e82c93aa1f34d5fb469f6512b559635
Taylor Plewe <tplewe@outlook.com>
2026-05-21 08:34:37 -0600

    translate Zed OS and architectures to the names I use in the bin files

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,30 @@ use zed_extension_api as zed;
 
 struct Canipls {}
 
+impl Canipls {
+    fn get_os_bin_word(os: zed::Os) -> String {
+        match os {
+            zed::Os::Windows => "windows".to_string(),
+            zed::Os::Linux => "linux".to_string(),
+            zed::Os::Mac => "macos".to_string(),
+        }
+    }
+
+    fn get_arch_bin_word(arch: zed::Architecture) -> String {
+        match arch {
+            zed::Architecture::X8664 => "x86_64".to_string(),
+            zed::Architecture::Aarch64 => "aarch64".to_string(),
+            _ => "".to_string(),
+        }
+    }
+
+    fn get_latest_canipls_version() {
+        let (os, arch) = zed::current_platform();
+        let bin_os = Canipls::get_os_bin_word(os);
+        let bin_arch = Canipls::get_arch_bin_word(arch);
+    }
+}
+
 impl zed::Extension for Canipls {
     fn new() -> Self
     where
