3cc4aeb2dfbf36af77f6537139270b19b6840fe9
Taylor Plewe <tplewe@outlook.com>
2026-05-23 13:27:23 -0600

    create SemVer struct, parse it

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,68 @@
+use std::fs;
+
 use zed_extension_api as zed;
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct SemVer {
+    major: usize,
+    minor: usize,
+    patch: usize,
+}
+enum SemVerError {
+    InvalidString,
+}
+impl SemVer {
+    fn from_string(sem_ver_string: &str) -> Result<Self, SemVerError> {
+        let mut sem_ver: SemVer = SemVer {
+            major: 0,
+            minor: 0,
+            patch: 0,
+        };
+
+        let cursor: usize = if sem_ver_string.starts_with('v') {
+            1
+        } else {
+            0
+        };
+        let mut it = sem_ver_string[cursor..].split('.');
+
+        // major
+        if let Some(major_str) = it.next() {
+            match major_str.parse::<usize>() {
+                Ok(major) => sem_ver.major = major,
+                Err(_) => return Err(SemVerError::InvalidString),
+            }
+        } else {
+            return Err(SemVerError::InvalidString);
+        }
+
+        // minor
+        if let Some(minor_str) = it.next() {
+            match minor_str.parse::<usize>() {
+                Ok(minor) => sem_ver.minor = minor,
+                Err(_) => return Err(SemVerError::InvalidString),
+            }
+        } else {
+            return Err(SemVerError::InvalidString);
+        }
+
+        // patch
+        if let Some(patch_str) = it.next() {
+            match patch_str.parse::<usize>() {
+                Ok(patch) => sem_ver.patch = patch,
+                Err(_) => return Err(SemVerError::InvalidString),
+            }
+        } else {
+            return Err(SemVerError::InvalidString);
+        }
+
+        return Ok(sem_ver);
+    }
+}
+
 struct Canipls {}
+
+const LATEST_FILENAME: &str = "latest";
 
 impl Canipls {
     fn get_os_bin_word(os: zed::Os) -> String {
@@ -19,10 +81,28 @@ impl Canipls {
         }
     }
 
-    fn get_latest_canipls_version() {
+    fn get_latest_canipls_version() -> Option<String> {
         let (os, arch) = zed::current_platform();
         let bin_os = Canipls::get_os_bin_word(os);
         let bin_arch = Canipls::get_arch_bin_word(arch);
+
+        let mut should_download_latest = false;
+
+        // compare against latest installed version
+        if let Ok(installed_version_str) = fs::read_to_string(LATEST_FILENAME) {
+        } else {
+            should_download_latest = true;
+        }
+
+        if let Ok(sem_ver) = SemVer::from_string("v1.2.3") {
+            // TODO: how to debug lol I can't find where this outputs to:
+            println!(
+                "sem ver: {}.{}.{}",
+                sem_ver.major, sem_ver.minor, sem_ver.patch
+            );
+        }
+
+        None
     }
 }
 
@@ -31,6 +111,7 @@ impl zed::Extension for Canipls {
     where
         Self: Sized,
     {
+        _ = Canipls::get_latest_canipls_version();
         Self {}
     }
 
